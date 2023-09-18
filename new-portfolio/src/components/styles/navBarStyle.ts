f8e263f07192cb3be34dcd13f055d6f484effffc
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-18 12:45:08 -0300

    fix: refactor navBar style

--- new-portfolio/src/components/styles/navBarStyle.ts
+++ new-portfolio/src/components/styles/navBarStyle.ts
@@ -1,32 +1,36 @@
 import styled from "styled-components";
 
-interface MenuProps {
-
-}
+interface MenuProps {}
 
 export const MenuItens = styled.nav`
-display: flex;
-flex-direction: row;
-align-items: center;
-gap: 2rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 2rem;
 
-@media (max-width: 768px) {
+  @media (max-width: 768px) {
     gap: 1rem;
     flex-direction: column;
     align-items: self-start;
-}
+  }
 `;
 
 export const NavLink = styled.a`
-  margin: 0.5rem;
-  color: #abb2bf;
+  color: var(--second-color);
   text-decoration: none;
+  font-weight: 400;
+  font-size: 16px;
+  
+  &:hover {
+    color: #ffffff;    
+  }
+
+  @media (max-width: 768px) {
+    font-size: 22px;
+  }
 
   span {
     color: #c778dd;
     margin: 0.3rem;
-  }
-  &:hover {
-    color: #FFFFFF;
   }
 `;
