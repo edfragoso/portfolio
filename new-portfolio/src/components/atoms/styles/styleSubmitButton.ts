66d7d23f608f4db72eb0e39eafd56b900e8daf5e
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-18 19:43:59 -0300

    feat: implement styleSubmitButton

---
+++ new-portfolio/src/components/atoms/styles/styleSubmitButton.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const SubButtom = styled.button`
+margin-top: 20px;
+  width: 10rem;
+  border: 1px solid #c778dd;
+  color: #fff;
+  font-size: 13px;
+  font-weight: 300;
+  padding: 0.6rem;
+  cursor: pointer;
+  background-color: #282c33;
+  transition: 0.2s ease-in-out;
+
+  &:hover {
+    font-weight: 600;
+    background-color: rgba(170, 134, 169, 0.2);
+    box-shadow: 0 0 3px 2px rgba(200, 80, 192, 0.6);
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+    /* transform: translateY(-2px); */
+  }
+`;
