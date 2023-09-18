e80e899d67524fa43ff802927eaa0c316b107fcc
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-18 12:43:09 -0300

    refactor: changed comp.

--- new-portfolio/src/app/page.tsx
+++ new-portfolio/src/app/page.tsx
@@ -1,9 +1,11 @@
-import { Header } from "@/components/common/header";
+"use client"
+import { Section } from "@/components/common/section";
+
 
 export default function Home() {
   return (
     <>
-      <Header />
+      <Section/>
     </>
   );
 }
