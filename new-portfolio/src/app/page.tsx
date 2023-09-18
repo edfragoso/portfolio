e4a5dc2cb7423a7172ef63fb0a7a94df8f99748d
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-18 19:41:50 -0300

    feat: implement DefaultPageLayoput

--- new-portfolio/src/app/page.tsx
+++ new-portfolio/src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 import { Section } from "@/components/common/section";
-
+import { DefaultPageLayout } from "@/components/styles/DefaultPageLayout";
 
 export default function Home() {
   return (
-    <>
+    <DefaultPageLayout>
       <Section/>
-    </>
+  
+    </DefaultPageLayout>
   );
 }
