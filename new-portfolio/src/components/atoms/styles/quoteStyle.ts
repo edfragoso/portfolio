b371f4992e465bbbb5015370d88a8256c0fd3e58
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-18 19:43:33 -0300

    feat: implement styleQuote

---
+++ new-portfolio/src/components/atoms/styles/quoteStyle.ts
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const BoxContent = styled.div`
+  padding: 1em;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const MarksLeft = styled.span`
+  background: transparent;
+  position: relative;
+  top: -25px;
+  left: 4px;
+
+  @media (max-width: 760px) {
+    top: -40px;
+    left: 4px;
+  }
+`;
+
+export const TextBox = styled.div`
+  border: solid 0.5px var(--second-color);
+  padding: 15px 0;
+  width: 100%;
+  display: flex;
+  text-align: center;
+  align-items: center;
+  font-size: 16px;
+  font-weight: 400;
+  color: var(--text-color);
+  font-style: italic;
+
+  @media (max-width: 760px) {
+    font-size: 14px;
+  }
+`;
+
+export const MarksRight = styled.span`
+  background: transparent;
+  position: relative;
+  top: 30px;
+  right: 5px;
+
+  @media (max-width: 760px) {
+    top: 50px;
+    right: 3px;
+  }
+`;
+
+export const BoxAuthor = styled.div`
+  border: solid 0.5px var(--second-color);
+  color: var(--text-color);
+  position: relative;
+  text-align: center;
+  padding: 0 8px;
+  right: -240px;
+  top: 0px;
+
+  @media (max-width: 760px) {
+    right: -50px;
+  }
+`;
