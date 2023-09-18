0fb4505ea28226d1968140c28f63e226c197a6b7
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-18 19:46:17 -0300

    feat: implement styleDefaultLayout

---
+++ new-portfolio/src/components/styles/DefaultPageLayout.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const DefaultPageLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 12px 24px;
+  min-height: 100vh;
+  background-color: var(--bg-primary);
+
+  @media (min-width: 768px) {
+    padding: 24px 30px;
+  }
+`;
