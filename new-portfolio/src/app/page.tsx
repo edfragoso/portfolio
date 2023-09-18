c5d9c16081a8b4819cd064117b45cd6ea271a52b
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-18 19:42:24 -0300

    feat: implement Cpmp. quote

--- new-portfolio/src/app/page.tsx
+++ new-portfolio/src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client"
+import { Quote } from "@/components/atoms/quote";
 import { Section } from "@/components/common/section";
 import { DefaultPageLayout } from "@/components/styles/DefaultPageLayout";
+
 
 export default function Home() {
   return (
     <DefaultPageLayout>
       <Section/>
-  
+      <Quote/>
     </DefaultPageLayout>
   );
 }
