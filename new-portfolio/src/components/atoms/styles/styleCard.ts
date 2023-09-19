7aab7a20b5c68faa2d5a79004302dd421e820b12
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-19 12:13:11 -0300

    feat: implement style section-project

---
+++ new-portfolio/src/components/atoms/styles/styleCard.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const SectionContent = styled.div`
+  border: solid 1px beige;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding: 0.5rem;
+  margin: 10px 30px;
+`;
+
+export const ImgBox = styled.div`
+  border: solid 1px green;
+  width: 100%;
+  background-color: aliceblue;
+`;
+
+export const TitleBox = styled.div`
+  border: solid 1px red;
+  padding: 0.5rem;
+  background-color: aqua;
+`;
+export const DescriptionBox = styled.div`
+  border: solid 1px blue;
+  padding: 0.5rem;
+  background-color: bisque;
+`;
