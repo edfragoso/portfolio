2fbeee8dd8184717452657f0bc707ab1c9f7da7e
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-19 12:11:41 -0300

    feat: created comp. Card

---
+++ new-portfolio/src/components/atoms/card.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { SubmitButton } from "./SubmitButton";
+import { DescriptionBox, ImgBox, SectionContent, TitleBox } from "./styles/styleCard";
+
+
+
+
+export function Card() {
+    return(
+        <SectionContent>
+            <ImgBox>imgUrl</ImgBox>
+            <TitleBox>title</TitleBox>
+            <DescriptionBox>description</DescriptionBox>
+            <SubmitButton title={"Visitar"}/>
+        </SectionContent>
+    );
+}
