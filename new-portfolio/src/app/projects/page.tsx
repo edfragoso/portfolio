743d2f3cdd9451820fe78aab6e52eff77176be89
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-19 12:11:23 -0300

    feat: created ProjectPage

---
+++ new-portfolio/src/app/projects/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+import styled from "styled-components";
+
+const Temp = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 200px;
+`;
+
+export default function Project() {
+  return (
+    <Temp>
+      <h1>🎯 - Pagina Projetos</h1>
+      <h3>🚧 - Em obras - 🚧</h3>
+    </Temp>
+  );
+}
