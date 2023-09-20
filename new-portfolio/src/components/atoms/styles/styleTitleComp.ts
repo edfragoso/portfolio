7412608ee212e297e5250a93c1f73656d68cadd9
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-20 17:42:50 -0300

    feat: implement styleTitleComp

---
+++ new-portfolio/src/components/atoms/styles/styleTitleComp.ts
@@ -0,0 +1,29 @@
+
+import styled from "styled-components";
+
+export const BoxContent = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+`;
+
+export const Marks = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  font-size: 16px;
+  margin-right: -3px;
+  font-weight: 200;
+  color: var(--primary-color);
+`;
+
+export const Title = styled.h3`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  font-size: 18px;
+ /*  font-style: italic; */
+  font-weight: 400;
+  color: var(--text-color);
+  margin-right: 5px;
+`;
