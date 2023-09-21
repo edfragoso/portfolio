bbba85f8f274719628905a3dd60f690708157715
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-21 16:33:19 -0300

    feat: created aboutSection comp.

---
+++ new-portfolio/src/components/common/aboutSection.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { SubmitButton } from "../atoms/SubmitButton";
+import { TitleComp } from "../atoms/TitleComp";
+import { BoxAbout, BoxContainer, BoxImg, BoxText } from "../styles/styleAboutSection";
+import eu from '../../../public/eu.png'
+
+export function About() {
+  return (
+    <BoxAbout>
+      <TitleComp title={"sobre"} markers={"#"} />
+      <BoxContainer>
+        <BoxText>
+          <p>Olá, sou Ednilson!</p>
+          <p>
+            Desenvolvedor Full Stack apaixonado por criar sites e aplicações envolventes.
+            Transformo ideias em soluções digitais inovadoras. Minha jornada
+            autodidata me levou a dominar tanto o front-end quanto o back-end.
+            Sempre atualizado com as últimas tecnologias. Pronto para construir
+            marcas online e enfrentar desafios. Vamos tornar suas ideias
+            realidade no mundo digital!
+          </p>
+          <SubmitButton title={"Leia mais"}/>
+        </BoxText>
+        <BoxImg>
+          <Image src={eu} alt={"eu"} width={300} height={300}/>
+        </BoxImg>
+      </BoxContainer>
+    </BoxAbout>
+  );
+}
