5da767bf28ab6d4224b6422028ae0cfd63fcd825
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-24 17:35:16 -0300

    fix: delete ssr

--- new-portfolio/next.config.js
+++ new-portfolio/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    ssr: {
-        styleSheets: {
-          async: true,
-        },
-      },
+    
 }
 
 module.exports = nextConfig
