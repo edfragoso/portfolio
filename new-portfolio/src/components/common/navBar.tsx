0740f9ee546dbc7da5734715b8362dd0c9cc17e6
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-24 17:02:08 -0300

    fix: add pathName

--- new-portfolio/src/components/common/navBar.tsx
+++ new-portfolio/src/components/common/navBar.tsx
@@ -1,53 +1,57 @@
-
 import Link from "next/link";
 import { MenuItens, NavLink } from "./styles/navBarStyle";
 import { useState } from "react";
 import { StyleSheetManager } from "styled-components";
+import { usePathname } from "next/navigation";
 
 export function NavBar({ closeMenu }: { closeMenu: () => void }) {
-  const [currentPath, setCurrentPath] = useState("/");
-
-  const handleChange = (newPath: string) => {
-    setCurrentPath(newPath);
+  const pathname = usePathname();
+  const [currentPath, setCurrentPath] = useState(pathname);
+    
+  const handleChange = (pathname: string) => {
+    setCurrentPath(pathname);
   };
-
+  
   const shouldForwardProp = (prop: string) => {
     return prop !== "isActive";
   };
 
   return (
     <StyleSheetManager shouldForwardProp={shouldForwardProp}>
-    <MenuItens>
-      <NavLink isActive={currentPath === "/"} onClick={() => handleChange("/")}>
-        <Link href={"/"} onClick={closeMenu}>
-          <span>#</span>Principal
-        </Link>
-      </NavLink>
-      <NavLink
-        isActive={currentPath === "/projects"}
-        onClick={() => handleChange("/projects")}
-      >
-        <Link href={"/projects"} onClick={closeMenu}>
-          <span>#</span>Projects
-        </Link>
-      </NavLink>
-      <NavLink
-        isActive={currentPath === "/about"}
-        onClick={() => handleChange("/about")}
-      >
-        <Link href={"/about"} onClick={closeMenu}>
-          <span>#</span>Sobre
-        </Link>
-      </NavLink>
-      <NavLink
-        isActive={currentPath === "/contact"}
-        onClick={() => handleChange("/contact")}
-      >
-        <Link href={"/contact"} onClick={closeMenu}>
-          <span>#</span>Contato
-        </Link>
-      </NavLink>
-    </MenuItens>
+      <MenuItens>
+        <NavLink
+          isActive={pathname === "/"}
+          onClick={() => handleChange(pathname)}
+        >
+          <Link href={"/"} onClick={closeMenu}>
+            <span>#</span>Principal
+          </Link>
+        </NavLink>
+        <NavLink
+          isActive={pathname === "/projects"}
+          onClick={() => handleChange(pathname)}
+        >
+          <Link href={"/projects"} onClick={closeMenu}>
+            <span>#</span>Projects
+          </Link>
+        </NavLink>
+        <NavLink
+          isActive={pathname === "/about"}
+          onClick={() => handleChange(pathname)}
+        >
+          <Link href={"/about"} onClick={closeMenu}>
+            <span>#</span>Sobre
+          </Link>
+        </NavLink>
+        <NavLink
+          isActive={pathname === "/contact"}
+          onClick={() => handleChange(pathname)}
+        >
+          <Link href={"/contact"} onClick={closeMenu}>
+            <span>#</span>Contato
+          </Link>
+        </NavLink>
+      </MenuItens>
     </StyleSheetManager>
   );
 }
