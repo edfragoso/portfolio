de38ca579260f8c7ef9215f82aa2865457462719
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-23 21:13:12 -0300

    feat: create contactPage

---
+++ new-portfolio/src/app/contact/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+import styled from "styled-components";
+
+const Temp = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 200px;
+`;
+
+export default function Contact() {
+  return (
+    <Temp>
+      <h1>🎯 - Pagina Contatos</h1>
+      <h3>🚧 - Em obras - 🚧</h3>
+    </Temp>
+  );
+}
