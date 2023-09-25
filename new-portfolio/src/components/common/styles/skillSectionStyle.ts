6e642ef0f400fb2a6bce1f8eeeb0651ff195dfcd
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-25 19:52:05 -0300

    refactor: changed structure and tagged template

--- new-portfolio/src/components/common/styles/skillSectionStyle.ts
+++ new-portfolio/src/components/common/styles/skillSectionStyle.ts
@@ -3,38 +3,19 @@ import styled from "styled-components";
 export const Container = styled.section`
   display: flex;
   align-items: center;
-  flex-direction: column;
+  flex-direction: row;
   flex-wrap: wrap;
   width: 100%;
   margin-top: 50px;
+  justify-content: space-around;
 
   @media (max-width: ${(props) => props.theme.desktopBreakpoint}) {
     margin-top: 30px;
-  }
-`;
-
-export const TitleContent = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  align-self: flex-start;
-  margin-bottom: 20px;
-`;
-
-export const BoxContainer = styled.article`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  justify-content: space-between;
-
-  @media (max-width: ${(props) => props.theme.desktopBreakpoint}) {
-    display: flex;
     flex-direction: column;
   }
 `;
 
-export const LeftBox = styled.div`
+export const LeftBox = styled.article`
   display: flex;
   align-items: center;
   flex-direction: row-reverse;
@@ -45,14 +26,15 @@ export const LeftBox = styled.div`
     text-align: center;
     font-weight: 600;
     color: var(--text-color);
+    margin-right: 20px;
   }
 `;
 
-export const RightBox = styled.div`
+export const RightBox = styled.article`
   display: flex;
   flex-wrap: wrap;
   align-items: flex-start;
-  width: 60%;
+  width: 50%;
   margin: 10px 10px;
 
   h4 {
