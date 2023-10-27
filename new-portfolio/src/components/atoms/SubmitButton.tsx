0a82b2a770987badc4729667378373e15b474d91
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-10-27 16:09:19 -0300

    refactor: remove param navigate in props

--- new-portfolio/src/components/atoms/SubmitButton.tsx
+++ new-portfolio/src/components/atoms/SubmitButton.tsx
@@ -5,13 +5,11 @@ import { useRouter } from "next/navigation";
 export interface SubmitButtonProps {
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
   title: string;
-  navigate: string;
 }
 
-export const SubmitButton = ({ title, onClick, navigate }: SubmitButtonProps) => {
+export const SubmitButton = ({ title, onClick }: SubmitButtonProps) => {
   const router = useRouter();
   const handleNavigate = () => {
-    router.push(navigate);
   };
   return (
     <SubButtom type="submit" onClick={handleNavigate}>
